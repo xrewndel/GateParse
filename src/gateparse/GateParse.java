f1b2beaf6eff12ae02021a8fc473d3e9286c8cf3
xrewndel <xrew@yandex.ru>
2014-11-04 20:31:03 +0400

    parse new way

--- src/gateparse/GateParse.java
+++ src/gateparse/GateParse.java
@@ -12,8 +12,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -23,33 +21,13 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class GateParse {
     private final static Logger csv = Logger.getLogger("csv");
+    private final static Logger main = Logger.getLogger("main");
     private static String path = "";
     private static List<Stat> requests = new ArrayList<Stat>();
-    private static List<Stat> responseBody = new ArrayList<Stat>();
+    private static List<Stat> responseBodies = new ArrayList<Stat>();
     private static String date = "";
-    private static String time = "";
-    private static Pattern r;
-    private static Pattern timeStamp;
-    
+
     public static void main(String[] args) {
-        // "03:05:27,499";
-        String timePattern = "\\d{2}:\\d{2}:\\d{2}\\,\\d{3}";
-        timeStamp = Pattern.compile(timePattern);
-        // "03:05:27,499  INFO PortSenderThreadPORT-103 etisalat-gate:run:157 - Port 103. Got Command = CommandSet{id=6846232, commands=[Command{body=*130*5462*20*0502712443*1#}]}";
-        String reqPattern = "(\\d{2}:\\d{2}:\\d{2}\\,\\d{3})(.*\\{)(.*)";
-        r = Pattern.compile(reqPattern);
-        
-        Matcher m = timeStamp.matcher("03:05:27,499");
-        if (m.find( )) {
-            System.out.println("Found value: " + m.group());
-            //System.out.println("Found value: " + m.group(2));
-            //System.out.println("Found value: " + m.group(3));
-        } else {
-           System.out.println("NO MATCH");
-        }
-        
-        //System.exit(1);
-        
         // settings for log
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
         System.setProperty("current.date", dateFormat.format(new Date()));
@@ -70,16 +48,17 @@ public class GateParse {
         File[] files = getFilesInDir(path);
         Set<File> flist = new TreeSet<File>(Arrays.asList(files));
         for (File file : flist) {
+            //clear(file.getAbsolutePath());
             read(file.getAbsolutePath());
         }
         System.out.println("Total files: " + files.length);
         System.out.println("After all:");
         for (Stat stat : requests) System.out.println(stat);
         System.out.println("");
-        for (Stat stat : responseBody) System.out.println(stat);
+        for (Stat stat : responseBodies) System.out.println(stat);
     }
     
-    private static File[] getFilesInDir(String path) {
+    public static File[] getFilesInDir(String path) {
         File[] files = new File(path).listFiles(new FilenameFilter() {
             @Override public boolean accept(File directory, String fileName) {
                 return fileName.startsWith("etisalat-gate.log");
@@ -90,109 +69,110 @@ public class GateParse {
     }
     
     // Чтение файла
-    private static List<Stat> read(String filename) {
-        System.out.println("Read " + filename);
-        date = filename.substring(filename.lastIndexOf(".") + 1);
+    private static void read(String fileName) {
+        System.out.println("Read " + fileName);
+        date = fileName.substring(fileName.lastIndexOf(".") + 1);
         System.out.println("DATE:" + date);
-        return readTxtFile(filename);
+        parse(fileName);
     }
     
-    private static List<Stat> readTxtFile(String filename) {
-        boolean add = false;
-        String currTime = "";
-        StringBuilder buffer = new StringBuilder();
+    private static void clear(String filename) {
+        String s0 = "EtisalatWorker-&566 etisalat-gate:printPorts";
+        String s1 = "EtisalatWorker";
         try {
             BufferedReader in = new BufferedReader(new FileReader(filename));
             while (in.ready()) {
                 String s = in.readLine();
-                if (!s.contains("EtisalatWorker")) {
-                    //System.out.println("Parse:" + s);
-                    Matcher m = timeStamp.matcher(s);
-                    if (m.find()) time = m.group();
-
-                    if (s.contains("body=*") || (s.contains("setUssdDwgResults") && !s.contains("Got Result") && !s.contains("Send"))) {
-                        Stat stat = new Stat(s);
-                        //System.out.println(stat);
-                        if (stat.isRequest()) requests.add(stat);
-                        else {
-                            boolean gotRequest = false;
-                            Iterator itReq = requests.listIterator();
-                            while(itReq.hasNext() && !gotRequest) {
-                                Stat request = (Stat) itReq.next();
-                                if (request.equals(stat)) {
-                                    Stat body = null;
-                                    if (stat.response.contains("OPERATION_NOT_SUPPORTED")) body = body();
-                                    
-                                    StringBuilder sb = new StringBuilder()
-                                      .append(date)             .append(";")
-                                      .append(request.time)     .append(";")
-                                      .append(stat.time)        .append(";")
-                                      .append(request.request)  .append(";")
-                                      .append(stat.response);//    .append("\n");
-                                    if (body != null) sb.append(";").append(body.msg);
-                                    csv.debug(sb);
-                                    itReq.remove();
-                                    gotRequest = true;
-                                }
-                            }
-                        }
-                    }
-                    
-                    if (s.contains("message = HEAD{")) { 
-                        add = true;
-                        buffer = new StringBuilder();
-                        
-                        //m = timeStamp.matcher(s);
-                        currTime = m.group();
-                    }
-                    if (!time.equals(currTime) && add) {
-                        buffer.append(s);
-                        add = false;
-                        // parse
-                        //System.out.println("Buffer:\n" + buffer + "\n");
-                        Stat stat = new Stat(buffer);
-                        if (!stat.msg.isEmpty())responseBody.add(stat);
-                        /*
-                        boolean gotRequest = false;
-                        Iterator it = requests.listIterator();
-                        while(it.hasNext() && !gotRequest) {
-                            Stat request = (Stat) it.next();
-                            if (request.equals(stat)) {
-                                StringBuilder sb = new StringBuilder()
-                                  .append(date)             .append(";")
-                                  .append(request.time)     .append(";")
-                                  .append(stat.time)        .append(";")
-                                  .append(request.request)  .append(";")
-                                  .append(stat.response)    .append(";")
-                                  .append(stat.message);
-
-                                csv.debug(sb);
-                                it.remove();
-                                gotRequest = true;
-                            }
-                        }
-                        */
-                    }
-
-                    if (add) buffer.append(s).append("\n");
-                    
-            }
+                if (!s.contains(s1)) main.debug(s);
             }
             in.close();
         } catch (Exception ex) { System.out.println(ex);  }
-
-        return new ArrayList<Stat>();
     }
     
-    private static Stat body() {
-        Stat body = null;
-        Iterator it = responseBody.iterator();
-        while(it.hasNext()) {
+    
+    private static void parse(String filename) {
+        StringBuilder bodyBuf = new StringBuilder();
+        boolean isBody = false;
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+            while (in.ready()) {
+                String s = in.readLine();
+                //System.out.println("Parse:" + s);
+                if (s.contains("body=*")) {                                     // request
+                    Stat request = Stat.request(s);
+                    requests.add(request);
+                }
+                else if(s.contains("message = HEAD{")) {                        // response body
+                    isBody = true;
+                    bodyBuf = new StringBuilder();
+                }
+                else if (s.contains("setUssdDwgResults") && s.contains("Got Result = ")) {  //response result
+                    // 19:11:53,291  INFO Thread-76 etisalat-gate:setUssdDwgResults:65 - Port 25. Got Result = 1
+                    int port = Integer.valueOf(s.substring(s.indexOf("Port ") + 5, s.indexOf(".")));
+                    int result = Integer.valueOf(s.substring(s.indexOf("=") + 1).trim());
+                    // идем по запросам и ищем запрос с таким портом
+                    Stat request = findRequest(port);
+                    //идем по ответам и ищем первый с таким результатом
+                    if (request != null) {
+                        Stat body = findBody(result);
+                        if (body != null) print(request, body);
+                    }
+                }
+                
+                if(isBody) {
+                    if (s.equals("}")) { // end
+                        isBody = false;
+                        Stat responseBody = Stat.body(bodyBuf);
+                        responseBodies.add(responseBody);
+                    }
+                    else bodyBuf.append(s).append("\n");
+                }
+                
+            }
+            in.close();
+        } catch (Exception ex) { System.out.println(ex);  }
+    }
+    
+    private static Stat findRequest(int port) {
+        Stat request = null;
+        boolean found = false;
+        Iterator it = requests.listIterator();
+        while (it.hasNext() && !found) {
             Stat stat = (Stat) it.next();
-            body = stat;
-            it.remove();
+            if (stat.port == port){
+                request = stat;
+                found = true;
+                it.remove();
+            }
+        }
+        
+        return request;
+    }
+    
+    private static Stat findBody(int result) {
+        Stat body = null;
+        boolean found = false;
+        Iterator it = responseBodies.listIterator();
+        while (it.hasNext() && !found) {
+            Stat stat = (Stat) it.next();
+            if (stat.result == result){
+                body = stat;
+                found = true;
+                it.remove();
+            }
         }
         
         return body;
+    }
+    
+    private static void print(Stat request, Stat response) {
+        StringBuilder sb = new StringBuilder()
+            .append(date)             .append(";")
+            .append(request.time)     .append(";")
+            .append(response.time)    .append(";")
+            .append(request.request)  .append(";")
+            .append(response.response).append(";")
+            .append(response.msg);
+          csv.debug(sb);
     }
 }
--- src/gateparse/Stat.java
+++ src/gateparse/Stat.java
@@ -11,66 +11,80 @@ public class Stat {
     public String response = "";
     public int thread = 0;
     public String msg = "";
+    public int result = -1;
     
-    public Stat(String s) { 
-        time = s.substring(0, s.indexOf(","));
+    private Stat() {}
+
+    //05:21:00,230  INFO PortSenderThreadPORT-16 etisalat-gate:run:157 - Port 16. Got Command = CommandSet{id=0, commands=[Command{body=*139*102*24512*5152*2#}]}
+    public static Stat request(String s) {
+        Stat stat = new Stat();
+        stat.time = getTime(s);
         int beginPort = s.indexOf("Port ") + 5;
         int endPort = s.indexOf(".", beginPort);
-        port = Integer.valueOf(s.substring(beginPort, endPort));
+        stat.port = Integer.valueOf(s.substring(beginPort, endPort));
+        stat.request = s.substring(s.indexOf("*"), s.indexOf("#"));
         
-        if (s.contains("setUssdDwgResults")) {
-            response = s.substring(s.indexOf(".") + 2);
-            int b = s.indexOf("Thread-") + 7;
-            int e = s.indexOf("etisalat") - 1;
-            thread = Integer.valueOf(s.substring(b, e));
+        return stat;
+   }
+    
+    public static Stat body(StringBuilder sb) {
+        Stat stat = new Stat();
+        String[] str = sb.toString().split("\n");
+        // 04:42:50,360  INFO Thread-70 etisalat-gate:run:115 - message = HEAD{
+        String s0 = str[0]; 
+        
+        stat.time = getTime(s0);
+        int b = s0.indexOf("-") + 1;
+        int e = s0.indexOf("etisalat") - 1;
+        stat.thread = Integer.valueOf(s0.substring(b, e));
+        
+        String body = sb.substring(sb.lastIndexOf("{") + 2).replaceAll("\n", " ");
+        /*
+        Body{
+        0
         }
-        else 
-            request = s.substring(s.indexOf("*"), s.indexOf("#"));
+        
+        BODY{
+        16
+        2
+        Recharging subscriber 971557957803 of 5 More Time completed through Operation 2256233824257. 
+        } 
+        
+        BODY{
+        5
+        1
+        Thank you for using Etisalat eRecharge Service. Your Airtime mWallet balance is 0.00 ID 40780540 on 02/11/14.
+        - - -
+        00:menu
+        0:<--
+        }
+        */
+        stat.msg = body;
+        
+        str = body.split(" ");
+        if (str.length == 1) stat.result = Integer.parseInt(str[0].trim());
+        else if (str.length >= 2) stat.result = Integer.parseInt(str[1].trim());
+        else System.out.println("not parsed: " + body);
+        
+        switch(stat.result) {
+            case 0: stat.response = "NO_FURTHER_USER_ACTION_REQUIRED";  break;
+            case 1: stat.response = "FURTHER_USER_ACTION_REQUIRED";     break;
+            case 2: stat.response = "USSD_TERMINATED_BY_NETWORK";       break;
+            case 3: stat.response = "";                                 break;
+            case 4: stat.response = "OPERATION_NOT_SUPPORTED";          break;
+            default: System.out.println("Unknown result code: " + stat.result);
+        }
+        
+        return stat;
     }
     
-    public Stat(StringBuilder sb) { 
-        try {
-            time = sb.substring(0, sb.indexOf(" "));
-            int b = sb.indexOf("-") + 1;
-            int e = sb.indexOf("etisalat") - 1;
-            thread = Integer.valueOf(sb.substring(b, e));
-
-            msg = sb.substring(sb.indexOf("{"), sb.indexOf("}") + 1);
-            msg = msg.replaceAll("\n", " ");
-            
-            if (!msg.contains("Operation not supported")) msg = "";
-            
-
-            /*String[] str = sb.toString().split("\n");
-            for (String s : str) {
-                if (s.contains("DEBUG") && s.contains("setUssdDwgResults")) {
-                    int beginPort = s.indexOf("Port ") + 5;
-                    int endPort = s.indexOf(".", beginPort);
-                    port = Integer.valueOf(s.substring(beginPort, endPort));
-                    response = s.substring(s.indexOf(".") + 2);
-                }
-            }*/
-        } catch(Exception ex) {
-            System.out.println("Parse:" + sb);
-            System.out.println(ex);
-        }
-         
-        if (thread ==0) {
-            System.out.println(sb);
-            System.out.println("---Thread: " + thread);
-        }
-        //System.out.println("---Time: " + time);
-        //System.out.println("---Thread: " + thread);
-        //System.out.println("---Message: " + message);
-        //System.out.println("---Port: " + port);
-        //System.out.println("---Response: " + response);
+    private static String getTime(String s) {
+        return s.substring(0, s.indexOf(","));
     }
-   
+    
     @Override public String toString() {
         return "Stat{" + "time=" + time + ", port=" + port + ", thread=" + thread + " request=" + request + ", response=" + response + ", msg=" + msg  + '}';
     }
-    
-    public boolean isRequest() { return !request.isEmpty(); }
     
     @Override
     public int hashCode() {
